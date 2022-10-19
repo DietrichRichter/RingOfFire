b8123e2527b474cec3bb88490b0c5c13de457690
Dima <dima@Dietrichs-Air.fritz.box>
2022-10-19 19:42:28 +0200

    add new arrays on games.js

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-game',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
+  pickCardAnimation = false;
+  currentCard?: string = '';
+  game!: Game;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+  }
+
+  takeCard() {
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      this.pickCardAnimation = true;
+      this.game.playedCards.push(this.currentCard);
+
+
+      setTimeout(() => {
+        this.pickCardAnimation = false;
+      }, 1500)
+    }
   }
 
 }
