93bff56b200ae634c1b26f3f84e996f8d5e882a9
Dima <dima@Dietrichs-Air.fritz.box>
2022-11-04 18:13:41 +0100

    fix buggs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,9 @@ export const environment = {
   firebase: {
     projectId: 'ringoffire-63e8d',
     appId: '1:950094603752:web:360a35b9773d16f220b05d',
+    databaseURL: 'https://ringoffire-63e8d-default-rtdb.europe-west1.firebasedatabase.app',
     storageBucket: 'ringoffire-63e8d.appspot.com',
+    locationId: 'europe-west',
     apiKey: 'AIzaSyDL6vb2yzuoL7tXb9syRPfNek7V5FsFkvs',
     authDomain: 'ringoffire-63e8d.firebaseapp.com',
     messagingSenderId: '950094603752',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,12 +4,14 @@
 
 export const environment = {
   firebase: {
-    apiKey: "AIzaSyDL6vb2yzuoL7tXb9syRPfNek7V5FsFkvs",
-    authDomain: "ringoffire-63e8d.firebaseapp.com",
-    projectId: "ringoffire-63e8d",
-    storageBucket: "ringoffire-63e8d.appspot.com",
-    messagingSenderId: "950094603752",
-    appId: "1:950094603752:web:a2e418f614e417f220b05d"
+    projectId: 'ringoffire-63e8d',
+    appId: '1:950094603752:web:360a35b9773d16f220b05d',
+    databaseURL: 'https://ringoffire-63e8d-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'ringoffire-63e8d.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDL6vb2yzuoL7tXb9syRPfNek7V5FsFkvs',
+    authDomain: 'ringoffire-63e8d.firebaseapp.com',
+    messagingSenderId: '950094603752',
   },
   production: false
 };
